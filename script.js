fd8688f1cedcf518c4122ea8357331f6c7794ed2
Dadams11 <adams.1743@osu.edu>
2023-06-21 22:55:22 -0400

    updated script.js file

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&ap
 
 var geoLon, geoLat;
 
+// Array to store city names
+var cityNames = [];
+
 function displayWeatherData(data) {
   var weatherInfoDiv = document.getElementById('weatherInfo');
   weatherInfoDiv.innerHTML = '';
@@ -13,17 +16,21 @@ function displayWeatherData(data) {
   if (data.list) {
     // Handle forecast weather data
     for (var forecast of data.list) {
+      //"2023-06-16 06:00:00"
+      if (forecast.dt_txt.slice(11, 13)==='12'){
+        console.log('date time', forecast.dt_txt.slice(11, 13))
       if (forecast.main && forecast.weather && forecast.weather.length > 0) {
         var dateTime = new Date(forecast.dt_txt);
         var date = dateTime.toDateString();
         var time = dateTime.toLocaleTimeString();
         var temperature = Math.round(forecast.main.temp - 273.15);
         var weatherDescription = forecast.weather[0].description;
-
+        var weatherIcon= `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
         var forecastDiv = document.createElement('div');
         forecastDiv.innerHTML = `
           <p>Date: ${date}</p>
           <p>Time: ${time}</p>
+          <img src= ${weatherIcon}>
           <p>Temperature: ${temperature}°C</p>
           <p>Description: ${weatherDescription}</p>
           <hr>
@@ -32,6 +39,7 @@ function displayWeatherData(data) {
         weatherInfoDiv.appendChild(forecastDiv);
       }
     }
+  }
   } else if (data.main && data.weather && data.weather.length > 0) {
     // Handle current weather data
     var dateTime = new Date(data.dt * 1000);
@@ -61,7 +69,7 @@ function getWeatherData(city) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      console.log('weather data', data);
       displayWeatherData(data);
     })
     .catch(function (error) {
@@ -100,6 +108,7 @@ function searchWeatherData() {
           })
           .then(function (data) {
             displayWeatherData(data);
+            console.log('weather data', data);
           })
           .catch(function (error) {
             console.log('Error:', error);
@@ -113,6 +122,7 @@ function searchWeatherData() {
     });
 }
 
+
 var searchButton = document.getElementById('searchButton');
 searchButton.addEventListener('click', function (event) {
   event.preventDefault();
@@ -120,10 +130,67 @@ searchButton.addEventListener('click', function (event) {
   city = cityInput.value.trim();
 
   if (city) {
+    //create an array to store city names
+
+    //save array into local storage
     localStorage.setItem('apiKey', apiKey);
     getWeatherData(city);
   }
 });
+// Function to retrieve data from local storage
+function retrieveDataFromLocalStorage() {
+  var storedData = localStorage.getItem('storedData');
+  if (storedData) {
+    return JSON.parse(storedData);
+  } else {
+    return [];
+  }
+}
+
+// Function to save data to local storage
+function saveDataToLocalStorage(data) {
+  localStorage.setItem('storedData', JSON.stringify(data));
+}
+
+// Function to create buttons for each item in local storage
+function createButtonsForLocalStorageData() {
+  var storedData = retrieveDataFromLocalStorage();
+  var buttonContainer = document.getElementById('buttonContainer');
+  buttonContainer.innerHTML = '';
+
+  storedData.forEach(function (item) {
+    var button = document.createElement('button');
+    button.textContent = item;
+    button.setAttribute('value', item);
+    button.addEventListener('click', function () {
+      var city = this.value;
+      getWeatherData(city);
+    });
+    buttonContainer.appendChild(button);
+  });
+}
+
+// Function to handle search button click event
+function handleSearchButtonClick() {
+  var cityInput = document.getElementById('cityInput');
+  city = cityInput.value.trim();
+
+  if (city) {
+    var storedData = retrieveDataFromLocalStorage();
+    storedData.push(city);
+    saveDataToLocalStorage(storedData);
+    createButtonsForLocalStorageData();
+    getWeatherData(city);
+  }
+}
+
+// Attach event listener to search button
+var searchButton = document.getElementById('searchButton');
+searchButton.addEventListener('click', handleSearchButtonClick);
+
+// Initial setup: Create buttons for existing data in local storage
+createButtonsForLocalStorageData();
+
 
 // Initialize with default city
 getCurrentWeatherData(city);
